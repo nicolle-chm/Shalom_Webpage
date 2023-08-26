a3ad658fe3f5aed38fea2e05260e8b97d36b4be6
Nicolle Chuquilin <135144946+nicolle-chm@users.noreply.github.com>
2023-08-26 23:56:21 +0100

    Update form-handler.php

--- form-handler.php
+++ form-handler.php
@@ -1,29 +1,32 @@
 <?php
-$name = $_POST['name'];
-$visitor_email = $_POST['email'];
-$subject = $_POST['subject'];
-$message = $_POST['message'];
+$name = htmlspecialchars($_POST['name']);
+$visitor_email = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) ? $_POST['email'] : '';
+$subject = htmlspecialchars($_POST['subject']);
+$message = htmlspecialchars($_POST['message']);
 
 $email_from = 'info@shalomlima.xyz';
-
 $email_subject = 'New Form Submission';
-
-$email_body = "User Name: $name.\n".
-"User Email: $visitor_email.\n".
-"Subject: $subject.\n".
-"User Message: $message.\n";
+$email_body = "User Name: $name\n".
+              "User Email: $visitor_email\n".
+              "Subject: $subject\n".
+              "User Message:\n$message\n";
 
 $to = 'nicolle.chm3@gmail.com';
+$headers = "From: $email_from\r\n" .
+           "Reply-to: $visitor_email\r\n" .
+           "Content-Type: text/plain; charset=UTF-8\r\n";
 
-$headers = "From: $email_from \r\n";
-
-$headers .= "Reply-to: $visitor_email \r\n";
-
-mail($to,$email_subject,$email_body,$headers);
-
-header("Location: contact.html");
-
+try {
+    if ($visitor_email !== '') {
+        mail($to, $email_subject, $email_body, $headers);
+        header("Location: contact.html");
+        exit();
+    } else {
+        echo "Invalid email address";
+    }
+} catch (Exception $e) {
+    echo "An error occurred: " . $e->getMessage();
+}
 ?>
-
 
 
